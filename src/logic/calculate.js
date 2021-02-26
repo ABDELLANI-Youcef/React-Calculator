f3dbb0b327ee569b419710be17a120c10e07505e
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-02-26 22:33:15 +0100

    Treate the case of buttonName in numbers catacter. use Big.js for both numbers in operate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,27 +1,42 @@
 import operate from './operate';
 
 const calculate = (calculator, buttonName) => {
-  let { total, next } = calculator;
-  switch (buttonName) {
-    case '+/-':
-      total *= -1;
-      next *= -1;
-      break;
-
-    case 'AC':
-      total = 0;
-      next = 0;
-      break;
-
-    case '=':
-      next = total;
-      break;
-
-    default:
-      total = operate(total, next, buttonName);
-      break;
+  let { total, next, operation } = calculator;
+  const nums = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  if (nums.includes(buttonName)) {
+    next = next === null ? buttonName : next + buttonName;
   }
-  return { total, next };
+  else {
+    switch (buttonName) {
+      case 'AC':
+        total = null;
+        next = null;
+        operation = null;
+        break;
+
+      case '+/-':
+        total = operate(total, '-1', 'X');
+        next = operate(next, '-1', 'X');
+        break;
+
+      case '=':
+        total = operate(total, next, operation);
+        next = null;
+        break;
+
+      default:
+        if (operation !== null) {
+          total = operate(total, next, operation);
+        }
+        else {
+          total = next;
+        }
+        operation = buttonName;
+        next = null;
+        break;
+    }
+  }
+  return { total, next, operation };
 };
 
 export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,21 +2,22 @@ import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
   const first = new Big(numberOne);
+  const second = new Big(numberTwo);
   switch (operation) {
     case '+':
-      return first.plus(numberTwo);
+      return first.plus(second).toString;
 
     case '-':
-      return first.minus(numberTwo);
+      return first.minus(second).toString;
 
     case 'X':
-      return first.times(numberTwo);
+      return first.times(second).toString;
 
     case '÷':
-      return first.div(numberTwo);
+      return first.div(second).toString;
 
     default:
-      return first.div(100).times(numberTwo);
+      return first.div(100).times(second).toString;
   }
 };
 
