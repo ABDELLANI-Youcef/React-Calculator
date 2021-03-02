49ac08856018cf90e96ccf0160277c5c7172124a
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-02 19:34:55 +0100

    Add style to the calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,26 @@
 import PropTypes from 'prop-types';
+import styles from '../style/Button.module.css';
 
-const Button = ({ name, clickHandler }) => {
+const Button = ({
+  name, clickHandler, wide, color,
+}) => {
   const handleClick = () => {
     clickHandler(name);
   };
+
+  const backgroud = color ? 'orange' : 'white';
+  const width = wide ? '50%' : '25%';
+  // const classList = wide ? [styles.zero, styles.button].join(' ') : styles.button;
+
   return (
-    <button type="button" onClick={handleClick}>{name}</button>
+    <button type="button" onClick={handleClick} className={styles.button} style={{ backgroundColor: backgroud, width }}>{name}</button>
   );
 };
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
+  wide: PropTypes.bool.isRequired,
+  color: PropTypes.bool.isRequired,
 };
 
 export default Button;
